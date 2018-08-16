bad23321b1568f734137abf78d87342278fb5096
Theo Mellske <blackvq35@gmail.com>
2018-08-15 23:23:36 -0500

    js functionality added to menu

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+// select dom items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-item');
+
+//set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+  if(!showMenu){
+    //if menu is closed we want to show these
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'));
+    showMenu = true;
+  }else{
+    //if menu is showing when button clicked, we want to remove classes
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+  }
+}
