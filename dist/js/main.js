7bec878030aa257fce2785c60b21e1e4f1749aae
Theo Mellske <blackvq35@gmail.com>
2018-08-16 22:09:47 -0500

    added css/js for hamburger rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,10 +13,12 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu(){
   if(!showMenu){
     //if menu is closed we want to show these
+    //first time its clicked upon site load it will get the close class
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
+    //loops through each menu item, renames it to single item and adds .show
     navItem.forEach(item => item.classList.add('show'));
     showMenu = true;
   }else{
